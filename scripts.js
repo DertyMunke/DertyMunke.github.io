cd1618dc06c92374af7d8ab15b2d476c3c288984
DertyMunke <joel.abney@gmail.com>
2016-03-04 18:21:05 -0800

    Latest version from eclipse

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+
+function popupOn(num) { 			
+    var popup = document.getElementById("popup" + num);	       
+    popup.style.display = "block";
+}
+
+function popupOff(num) { 			
+    var popup = document.getElementById("popup" + num);	       
+    popup.style.display = "none";
+}
